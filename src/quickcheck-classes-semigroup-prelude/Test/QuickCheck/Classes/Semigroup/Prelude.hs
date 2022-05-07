5f02fd5bf7d07efc1e9070cc7d991a68c1c3b670
Jonathan Knowles <mail@jonathanknowles.net>
2022-05-07 06:46:22 +0000

    Add `Prelude` module for ease of testing with `ghci`.

---
+++ src/quickcheck-classes-semigroup-prelude/Test/QuickCheck/Classes/Semigroup/Prelude.hs
@@ -0,0 +1,19 @@
+-- |
+-- Copyright: © 2022 Jonathan Knowles
+-- License: Apache-2.0
+--
+module Test.QuickCheck.Classes.Semigroup.Prelude
+    ( module Data.Monoid
+    , module Numeric.Natural
+    , module Test.QuickCheck
+    , module Test.QuickCheck.Classes.Semigroup
+    , module Test.QuickCheck.Classes.Semigroup.Tuple
+    )
+    where
+
+import Data.Monoid
+import Numeric.Natural
+import Test.QuickCheck
+import Test.QuickCheck.Classes.Semigroup
+import Test.QuickCheck.Classes.Semigroup.Tuple
+import Test.QuickCheck.Instances.Natural ()
