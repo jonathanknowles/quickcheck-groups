83e6b5fc47533816d596ef9500f6cc7ac480ab9d
Jonathan Knowles <mail@jonathanknowles.net>
2022-05-07 23:22:43 +0000

    Add imports from `monoid-subclasses` to `Prelude` module.

--- src/quickcheck-classes-semigroup-prelude/Test/QuickCheck/Classes/Semigroup/Prelude.hs
+++ src/quickcheck-classes-semigroup-prelude/Test/QuickCheck/Classes/Semigroup/Prelude.hs
@@ -4,6 +4,11 @@
 --
 module Test.QuickCheck.Classes.Semigroup.Prelude
     ( module Data.Monoid
+    , module Data.Monoid.Cancellative
+    , module Data.Monoid.GCD
+    , module Data.Monoid.Monus
+    , module Data.Monoid.Null
+    , module Data.Semigroup.Cancellative
     , module Numeric.Natural
     , module Test.QuickCheck
     , module Test.QuickCheck.Classes.Semigroup
@@ -12,6 +17,11 @@ module Test.QuickCheck.Classes.Semigroup.Prelude
     where
 
 import Data.Monoid
+import Data.Monoid.Cancellative
+import Data.Monoid.GCD
+import Data.Monoid.Monus
+import Data.Monoid.Null
+import Data.Semigroup.Cancellative
 import Numeric.Natural
 import Test.QuickCheck
 import Test.QuickCheck.Classes.Semigroup
