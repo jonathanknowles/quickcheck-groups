3971362c259029e12d3d8255d43acecbfae3f8fd
Jonathan Knowles <mail@jonathanknowles.net>
2022-09-17 05:11:47 +0000

    Remove everything unrelated to groups.

--- src/quickcheck-groups-test/Test/QuickCheck/Classes/SemigroupSpec.hs
+++ src/quickcheck-groups-test/Test/QuickCheck/Classes/SemigroupSpec.hs
@@ -6,18 +6,8 @@
 --
 module Test.QuickCheck.Classes.SemigroupSpec where
 
-import Data.ByteString.Lazy
-    ( ByteString )
-import Data.Map.Strict
-    ( Map )
 import Data.Monoid
-    ( Product (..), Sum (..) )
-import Data.Set
-    ( Set )
-import Data.Text
-    ( Text )
-import Numeric.Natural
-    ( Natural )
+    ( Sum (..) )
 import Test.Hspec
     ( Spec )
 import Test.QuickCheck
@@ -26,26 +16,8 @@ import Test.QuickCheck.Classes
     ( Laws (..) )
 import Test.QuickCheck.Classes.Hspec
     ( testLawsMany )
-import Test.QuickCheck.Classes.Monoid.GCD
-    ( cancellativeGCDMonoidLaws
-    , gcdMonoidLaws
-    , leftGCDMonoidLaws
-    , overlappingGCDMonoidLaws
-    , rightGCDMonoidLaws
-    )
-import Test.QuickCheck.Classes.Monoid.Null
-    ( monoidNullLaws, positiveMonoidLaws )
 import Test.QuickCheck.Classes.Semigroup
-    ( groupLaws, monusLaws )
-import Test.QuickCheck.Classes.Semigroup.Cancellative
-    ( cancellativeLaws
-    , commutativeLaws
-    , leftCancellativeLaws
-    , leftReductiveLaws
-    , reductiveLaws
-    , rightCancellativeLaws
-    , rightReductiveLaws
-    )
+    ( groupLaws )
 import Test.QuickCheck.Instances.ByteString
     ()
 import Test.QuickCheck.Instances.Natural
@@ -58,149 +30,10 @@ import Test.QuickCheck.Property
 spec :: Spec
 spec = do
     testLawsMany @() $ disableCoverageCheck
-        [ cancellativeGCDMonoidLaws
-        , cancellativeLaws
-        , commutativeLaws
-        , gcdMonoidLaws
-        , groupLaws
-        , leftCancellativeLaws
-        , leftGCDMonoidLaws
-        , leftReductiveLaws
-        , monoidNullLaws
-        , monusLaws
-        , overlappingGCDMonoidLaws
-        , positiveMonoidLaws
-        , reductiveLaws
-        , rightCancellativeLaws
-        , rightGCDMonoidLaws
-        , rightReductiveLaws
-        ]
-    testLawsMany @ByteString
-        [ leftCancellativeLaws
-        , leftGCDMonoidLaws
-        , leftReductiveLaws
-        , monoidNullLaws
-        , overlappingGCDMonoidLaws
-        , positiveMonoidLaws
-        , rightCancellativeLaws
-        , rightGCDMonoidLaws
-        , rightReductiveLaws
-        ]
-    testLawsMany @Text
-        [ leftCancellativeLaws
-        , leftGCDMonoidLaws
-        , leftReductiveLaws
-        , monoidNullLaws
-        , overlappingGCDMonoidLaws
-        , positiveMonoidLaws
-        , rightCancellativeLaws
-        , rightGCDMonoidLaws
-        , rightReductiveLaws
-        ]
-    testLawsMany @[Int]
-        [ leftCancellativeLaws
-        , leftGCDMonoidLaws
-        , leftReductiveLaws
-        , monoidNullLaws
-        , overlappingGCDMonoidLaws
-        , positiveMonoidLaws
-        , rightCancellativeLaws
-        , rightGCDMonoidLaws
-        , rightReductiveLaws
-        ]
-    testLawsMany @(Set Int)
-        [ commutativeLaws
-        , gcdMonoidLaws
-        , leftGCDMonoidLaws
-        , leftReductiveLaws
-        , monoidNullLaws
-        , monusLaws
-        , overlappingGCDMonoidLaws
-        , positiveMonoidLaws
-        , reductiveLaws
-        , rightGCDMonoidLaws
-        , rightReductiveLaws
-        ]
-    testLawsMany @(Set Natural)
-        [ commutativeLaws
-        , gcdMonoidLaws
-        , leftGCDMonoidLaws
-        , leftReductiveLaws
-        , monoidNullLaws
-        , monusLaws
-        , overlappingGCDMonoidLaws
-        , positiveMonoidLaws
-        , reductiveLaws
-        , rightGCDMonoidLaws
-        , rightReductiveLaws
-        ]
-    testLawsMany @(Product Int)
-        [ commutativeLaws
-        , leftReductiveLaws
-        , monoidNullLaws
-        , reductiveLaws
-        , rightReductiveLaws
-        ]
-    testLawsMany @(Product Natural)
-        [ commutativeLaws
-        , gcdMonoidLaws
-        , leftGCDMonoidLaws
-        , leftReductiveLaws
-        , monoidNullLaws
-        , monusLaws
-        , overlappingGCDMonoidLaws
-        , positiveMonoidLaws
-        , reductiveLaws
-        , rightGCDMonoidLaws
-        , rightReductiveLaws
+        [ groupLaws
         ]
     testLawsMany @(Sum Int)
-        [ cancellativeLaws
-        , commutativeLaws
-        , groupLaws
-        , leftCancellativeLaws
-        , leftReductiveLaws
-        , monoidNullLaws
-        , reductiveLaws
-        , rightCancellativeLaws
-        , rightReductiveLaws
-        ]
-    testLawsMany @(Sum Natural)
-        [ cancellativeGCDMonoidLaws
-        , cancellativeLaws
-        , commutativeLaws
-        , gcdMonoidLaws
-        , leftCancellativeLaws
-        , leftGCDMonoidLaws
-        , leftReductiveLaws
-        , monoidNullLaws
-        , monusLaws
-        , overlappingGCDMonoidLaws
-        , positiveMonoidLaws
-        , reductiveLaws
-        , rightCancellativeLaws
-        , rightGCDMonoidLaws
-        , rightReductiveLaws
-        ]
-    testLawsMany @(Map Int Int)
-        [ leftGCDMonoidLaws
-        , leftReductiveLaws
-        , monoidNullLaws
-        -- TODO:
-        -- Determine why these laws are not satisfied:
-        -- overlappingGCDMonoidLaws
-        , positiveMonoidLaws
-        , rightReductiveLaws
-        ]
-    testLawsMany @(Map Int Natural)
-        [ leftGCDMonoidLaws
-        , leftReductiveLaws
-        , monoidNullLaws
-        -- TODO:
-        -- Determine why these laws are not satisfied:
-        -- overlappingGCDMonoidLaws
-        , positiveMonoidLaws
-        , rightReductiveLaws
+        [ groupLaws
         ]
 
 --------------------------------------------------------------------------------
--- src/quickcheck-groups/Test/QuickCheck/Classes/Semigroup.hs
+++ src/quickcheck-groups/Test/QuickCheck/Classes/Semigroup.hs
@@ -5,27 +5,10 @@
 -- Copyright: © 2022 Jonathan Knowles
 -- License: Apache-2.0
 --
--- This module provides:
---
--- * Support for testing 'Semigroup' subclass instances with QuickCheck.
--- * Reusable properties in the form of 'Laws' definitions.
---
--- In general:
---
--- * Each 'Laws' definition includes properties that relate to __just one__
---   __class__.
--- * Laws for superclasses are __not__ transitively included.
---
--- Therefore, when testing laws for a particular class, you should make sure to
--- also test laws for all superclasses.
---
 module Test.QuickCheck.Classes.Semigroup
     (
-    -- * Monus
-      monusLaws
-
     -- * Group
-    , groupLaws
+      groupLaws
     )
     where
 
@@ -34,10 +17,6 @@ import Prelude hiding
 
 import Data.Group
     ( Group (..) )
-import Data.Monoid.GCD
-    ( OverlappingGCDMonoid (..) )
-import Data.Monoid.Monus
-    ( Monus (..) )
 import Data.Proxy
     ( Proxy (..) )
 import Test.QuickCheck
@@ -157,50 +136,3 @@ groupLaw_pow_nonPositive a =
     makeProperty
         "pow a n == invert (mconcat (replicate (abs n) a))"
         (pow a n == invert (mconcat (replicate (abs n) a)))
-
---------------------------------------------------------------------------------
--- Monus
---------------------------------------------------------------------------------
-
--- | 'Laws' for instances of 'Monus'.
---
--- Tests the following properties:
---
--- prop> a <\> b == stripPrefixOverlap b a
--- prop> a <\> b == stripSuffixOverlap b a
---
--- Note that the following superclass laws are __not__ included:
---
--- * 'commutativeLaws'
--- * 'overlappingGCDMonoidLaws'
---
-monusLaws
-    :: forall a. (Arbitrary a, Show a, Eq a, Monus a)
-    => Proxy a
-    -> Laws
-monusLaws _ = Laws "Monus"
-    [ makeLaw2 @a
-        "monusLaw_stripPrefixOverlap"
-        (monusLaw_stripPrefixOverlap)
-    , makeLaw2 @a
-        "monusLaw_stripSuffixOverlap"
-        (monusLaw_stripSuffixOverlap)
-    ]
-
-monusLaw_stripPrefixOverlap
-    :: (Eq a, Monus a) => a -> a -> Property
-monusLaw_stripPrefixOverlap a b =
-    makeProperty
-        "a <\\> b == stripPrefixOverlap b a"
-        (a <\\> b == stripPrefixOverlap b a)
-  where
-    (<\\>) = (<\>)
-
-monusLaw_stripSuffixOverlap
-    :: (Eq a, Monus a) => a -> a -> Property
-monusLaw_stripSuffixOverlap a b =
-    makeProperty
-        "a <\\> b == stripSuffixOverlap b a"
-        (a <\\> b == stripSuffixOverlap b a)
-  where
-    (<\\>) = (<\>)
