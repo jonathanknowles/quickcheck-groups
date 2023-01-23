d7d122a2ca8abd504423a9509588926ebe6ed0fe
Jonathan Knowles <mail@jonathanknowles.net>
2023-01-23 13:49:08 +0000

    Update copyright messages.

--- src/internal/Data/Semigroup/Eq.hs
+++ src/internal/Data/Semigroup/Eq.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE DataKinds #-}
 
 -- |
--- Copyright: © 2022 Jonathan Knowles
+-- Copyright: © 2022–2023 Jonathan Knowles
 -- License: Apache-2.0
 --
 module Data.Semigroup.Eq
--- src/public/Test/QuickCheck/Classes/Group.hs
+++ src/public/Test/QuickCheck/Classes/Group.hs
@@ -2,7 +2,7 @@
 {- HLINT ignore "Redundant bracket" -}
 
 -- |
--- Copyright: © 2022 Jonathan Knowles
+-- Copyright: © 2022–2023 Jonathan Knowles
 -- License: Apache-2.0
 --
 module Test.QuickCheck.Classes.Group
