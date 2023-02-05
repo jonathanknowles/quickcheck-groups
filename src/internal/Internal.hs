48716866239d002ef6f26831ed886435a236674d
Jonathan Knowles <mail@jonathanknowles.net>
2023-02-05 03:43:10 +0000

    Miscellaneous improvements to `internal` library.
    The `internal` library within `quickcheck-groups` was originally a fork
    of the `internal` library within `quickcheck-monoid-subclasses`.
    
    This commit brings the `internal` library up to date with recent changes
    made to the `quickcheck-monoid-subclasses' library.
    
    In future, we should consider extracting out this `internal` library to
    a separate library on hackage, to avoid code duplication.

--- src/internal/Internal.hs
+++ src/internal/Internal.hs
@@ -3,11 +3,14 @@
 -- License: Apache-2.0
 --
 module Internal
-    ( makeLaw0
+    ( cover
+    , makeLaw0
     , makeLaw1
     , makeLaw2
     , makeLaw3
     , makeProperty
+    , report
+    , (==>)
     )
     where
 
@@ -16,7 +19,7 @@ import Data.Function
 import Data.Proxy
     ( Proxy (..) )
 import Internal.Semigroup.Eq
-    ( allUnique, canVerifyAllNonNull )
+    ( allNonNull, allUnique, allUniqueNonNull )
 import Internal.Semigroup.Tuple
     ( Tuple1, Tuple2, Tuple3, evalTuple1, evalTuple2, evalTuple3 )
 import Test.QuickCheck
@@ -25,22 +28,29 @@ import Test.QuickCheck
     , Testable
     , checkCoverage
     , counterexample
-    , cover
     , property
     )
+
+import qualified Test.QuickCheck as QC
+
+infixr 0 ==>
+(==>) :: Bool -> Bool -> Bool
+a ==> b = not a || b
+
+cover :: Testable t => String -> Bool -> t -> Property
+cover name = flip (QC.cover 0.1) (replaceSpecialChars <$> name)
 
 makeLaw :: Testable t => String -> t -> (String, Property)
 makeLaw title t = (title, checkCoverage $ property t)
 
 makeLaw0
-    :: forall a. (Eq a, Monoid a)
-    => String
+    :: String
     -> (Proxy a -> Property)
     -> (String, Property)
 makeLaw0 s = makeLaw s . makeProperty0
 
 makeLaw1
-    :: (Arbitrary a, Show a, Eq a, Monoid a, Testable t)
+    :: (Arbitrary a, Show a, Eq a, Semigroup a, Testable t)
     => String
     -> (a -> t)
     -> (String, Property)
@@ -70,9 +80,6 @@ makeProperty propertyDescription t =
             & fmap replaceSpecialChars
         ]
       where
-        replaceSpecialChars = \case
-            'λ'   -> '\\'
-            other -> other
 
 makeProperty0
     :: forall a t. Testable t
@@ -81,28 +88,26 @@ makeProperty0
 makeProperty0 p = property $ p $ Proxy @a
 
 makeProperty1
-    :: (Eq a, Monoid a, Testable t)
+    :: (Eq a, Semigroup a, Testable t)
     => (a -> t)
     -> (Tuple1 a -> Property)
 makeProperty1 p (evalTuple1 -> a)
-    = cover 1 (a == mempty) "a == mempty"
-    $ cover 1 (a /= mempty) "a /= mempty"
-    $ property $ p a
+    = property $ p a
 
 makeProperty2
     :: (Eq a, Semigroup a, Testable t)
     => (a -> a -> t)
     -> (Tuple2 a -> Property)
 makeProperty2 p (evalTuple2 -> (a, b))
-    = cover 1
-        (allUnique [a, b])
+    = cover
         "allUnique [a, b]"
-    $ cover 1
-        (canVerifyAllNonNull [a, b])
-        "canVerifyAllNonNull [a, b]"
-    $ cover 1
-        (allUnique [a, b] && canVerifyAllNonNull [a, b])
-        "allUnique [a, b] && canVerifyAllNonNull [a, b]"
+        (allUnique [a, b])
+    $ cover
+        "allNonNull [a, b]"
+        (allNonNull [a, b])
+    $ cover
+        "allUniqueNonNull [a, b]"
+        (allUniqueNonNull [a, b])
     $ property $ p a b
 
 makeProperty3
@@ -110,13 +115,22 @@ makeProperty3
     => (a -> a -> a -> t)
     -> (Tuple3 a -> Property)
 makeProperty3 p (evalTuple3 -> (a, b, c))
-    = cover 1
-        (allUnique [a, b, c])
+    = cover
         "allUnique [a, b, c]"
-    $ cover 1
-        (canVerifyAllNonNull [a, b, c])
-        "canVerifyAllNonNull [a, b, c]"
-    $ cover 1
-        (allUnique [a, b, c] && canVerifyAllNonNull [a, b, c])
-        "allUnique [a, b, c] && canVerifyAllNonNull [a, b, c]"
+        (allUnique [a, b, c])
+    $ cover
+        "allNonNull [a, b, c]"
+        (allNonNull [a, b, c])
+    $ cover
+        "allUniqueNonNull [a, b, c]"
+        (allUniqueNonNull [a, b, c])
     $ property $ p a b c
+
+report :: (Show a, Testable prop) => String -> a -> prop -> Property
+report name a = counterexample $
+    (replaceSpecialChars <$> name) <> ":\n" <> show a <> "\n"
+
+replaceSpecialChars :: Char -> Char
+replaceSpecialChars = \case
+    'λ'   -> '\\'
+    other -> other
--- src/internal/Internal/Semigroup/Eq.hs
+++ src/internal/Internal/Semigroup/Eq.hs
@@ -1,13 +1,11 @@
-{-# LANGUAGE ConstraintKinds #-}
-{-# LANGUAGE DataKinds #-}
-
 -- |
 -- Copyright: © 2022–2023 Jonathan Knowles
 -- License: Apache-2.0
 --
 module Internal.Semigroup.Eq
-    ( allUnique
-    , canVerifyAllNonNull
+    ( allNonNull
+    , allUnique
+    , allUniqueNonNull
     )
     where
 
@@ -16,10 +14,16 @@ import Data.List
 
 import Data.Foldable as F
 
+allNonNull :: (Eq a, Semigroup a, Foldable f) => f a -> Bool
+allNonNull as = F.all (as `canVerifyNonNull`) as
+
 allUnique :: (Eq a, Foldable f) => f a -> Bool
 allUnique as = length (nub xs) == length xs
   where
     xs = F.toList as
+
+allUniqueNonNull :: (Eq a, Foldable f, Semigroup a) => f a -> Bool
+allUniqueNonNull as = allUnique as && allNonNull as
 
 canModify :: (Eq a, Semigroup a) => a -> a -> Bool
 a `canModify` b = (||)
@@ -31,9 +35,6 @@ a `canModifyL` b = b /= a <> b
 
 canModifyR :: (Eq a, Semigroup a) => a -> a -> Bool
 a `canModifyR` b = b /= b <> a
-
-canVerifyAllNonNull :: (Eq a, Semigroup a, Foldable f) => f a -> Bool
-canVerifyAllNonNull as = F.all (as `canVerifyNonNull`) as
 
 canVerifyNonNull :: (Eq a, Semigroup a, Foldable f) => f a -> a -> Bool
 canVerifyNonNull as a = F.any (a `canModify`) as
